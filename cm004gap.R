25ddfd604db1c637bb0e8e5ef2b38c754cfca450
Sarah Dier-McComb <sarah.mccomb8@gmail.com>
2017-09-14 12:33:16 -0700

    data things, pt 2 of T04

---
+++ cm004gap.R
@@ -0,0 +1,20 @@
+##
+
+install.packages("gapminder") #caught up at 10:30 in class, poor notes - re-read instructions to follow
+install.packages("tidyverse")
+library(gapminder)
+library(tidyverse)
+
+
+head(gapminder)
+tail(gapminder)
+str(gapminder)
+summary(gapminder)
+glimpse(gapminder)
+
+diag(5) #matrix of one data type 
+
+tibble() #dont know what does, breifly wrote 'would see the whole thing exicuted. 
+#reg data frame - would be way too long
+
+#always plot the data. 
